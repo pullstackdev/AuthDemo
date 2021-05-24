9afbd6a3c8ffbd50add136f67f4fbdd1f9b95911
web net digi <webnetdigi@outlook.com>
2021-05-24 13:58:44 +0300

    identity and jwt

---
+++ JWT-MVC/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using JWT_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JWT_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            // a sample jwt encoded token string which is supposed to be extracted from 'Authorization' HTTP header in your Web Api controller
+            var tokenString = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjpbIjdmN2UwZTVmLTUyMmMtNGVlYy05MTFkLTFmOGEzZTU0OTM0ZiIsImFkbWluIl0sImV4cCI6MTYyMjQ0OTI4NiwiaXNzIjoiVG9rZW5BdXRoRGVtbyIsImF1ZCI6IlRva2VuQXV0aERlbW8ifQ.vAZZff3yEoNz78lguxr3AlelevwjQyqr4l-jBLj_NsU";
+            var jwtEncodedString = tokenString.Substring(7); // trim 'Bearer ' from the start since its just a prefix for the token string
+
+            var token = new JwtSecurityToken(jwtEncodedString: jwtEncodedString);
+            Console.WriteLine("email => " + token.Claims.First(c => c.Type == "Name").Value);
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ coreIdentity/Controllers/LoginController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace coreIdentity.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(string text)
+        {
+            List<string> list = new List<string>
+            {
+                "1","2"
+            };
+
+            if (list.Contains(text)) //kullanıcı kayıtlı ise
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, text) //Claim burada type ve value alacak, claim'e bu user'ı atadık
+                };
+                var userIdentity = new ClaimsIdentity(claims, "Login"); //claim'i ve type'ı istedi
+                ClaimsPrincipal principal = new ClaimsPrincipal(userIdentity);
+                await HttpContext.SignInAsync(principal); //şifreli bir cookie oluşturur
+                return RedirectToAction("Index", "Home"); //giriş yapabileni category'e gönder
+            }
+            return View(); //kayıtlı olmayan user tekrar burada
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme); //girerken oluşturulan şifreli cookie'yi signout yapıyoruz
+            return RedirectToAction("Index", "Login");
+        }
+    }
+}
